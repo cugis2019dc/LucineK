175b0abea57e881ce9e96adcb3681f5aa7d8f682
lkamara01 <53440506+lkamara01@users.noreply.github.com>
2019-07-29 13:55:37 -0400

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+print("Hello, my name is Lucine")
+
+5*2
+5/2
+5**2
+8/9*3
+print("5*2")
+
+def multiply(a,b):
+    multiply = a*b
+    
+    print(multiply)
+
+def sum(a,b,c):
+    sum = a+b+c
+    
+    print(sum)
+    
+sum(1,2,3)
+
+def area(a,b,c):
+    area = a*b*c
+    
+    print(area)
+
+area(1/2,3,2)
+
+def area(b,h):
+    area = (0.5)*b*h
+    
+    print("the area of the triangle is", area)
+    
+area(3,2)
+
+def area(a,b,c):
+    area = (0.5)*b+c*a
+    
+    print("the area of the trapezoid is", area)
+    
+area(2,3,4)
+
+def triArea(b,h):
+    triArea=.5*b*h
+    print('the area of the triangle with the base of',b, 'and a height of',h, 'is' triArea)
+    
+triArea(5,6)
+
+
+    
+    
+    
+    
